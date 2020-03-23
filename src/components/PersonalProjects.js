67a0de7e4cca249653611c18bc052819db80b28e
malarm <malarm09@gmail.com>
2020-03-23 13:48:42 +0100

    Update PersonalProjects.js

--- src/components/PersonalProjects.js
+++ src/components/PersonalProjects.js
@@ -9,7 +9,10 @@ const PersonalProjects = () => {
                 <Card shadow={5} className="container">
                     <CardTitle>Search and Display data from API</CardTitle>
                     <CardText>
-                        <p> This website is made for a company which rents out summer houses to its clients. There are two main features in this website.</p>
+                        <p> This website is made for a company which rents out summer houses to its clients.
+                        There are two main features in this website...<span><a href="https://github.com/malarm/React-With-API" target="_blank" className="more"  >More</a></span>
+
+                        </p>
                     </CardText>
                     <CardActions>
                         <button type="submit"><a href="https://github.com/malarm/React-With-API" target="_blank" className="github-link">GitHub</a></button>
@@ -19,7 +22,10 @@ const PersonalProjects = () => {
                 <Card shadow={5} className="container" >
                     <CardTitle >Malar Resort</CardTitle>
                     <CardText>
-                        <p> This website is made for a company which rents out summer houses to its clients. There are two main features in this website.</p>
+                        <p> I have developed a website for a resort.
+                        The search function is the main function of the website.
+                        Hotel rooms can be selected based on room type, number of guests...
+                            <span><a href="https://github.com/malarm/malarresort" target="_blank" className="more"  >More</a></span></p>
                     </CardText>
                     <CardActions>
                         <button type="submit"><a href="https://github.com/malarm/malarresort" target="_blank" className="github-link">GitHub</a></button>
@@ -29,7 +35,7 @@ const PersonalProjects = () => {
                 <Card className="container" >
                     <CardTitle >Search Movies from IMDB</CardTitle>
                     <CardText>
-                        <p> This website is made for a company which rents out summer houses to its clients. There are two main features in this website.</p>
+                        <p> I have developed a website to learn to use web API. It was to challenge myself as I never worked with web API before...<span><a href="https://github.com/malarm/IMDB" target="_blank" className="more"  >More</a></span></p>
                     </CardText>
                     <CardActions>
                         <button type="submit"><a href="https://github.com/malarm/IMDB" target="_blank" className="github-link">GitHub</a></button>
