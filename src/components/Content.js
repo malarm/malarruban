9eeb5956c8854fa9da0c817057346c05b15b8ba3
malarm <malarm09@gmail.com>
2020-04-16 23:02:42 +0200

    Updated description

--- src/components/Content.js
+++ src/components/Content.js
@@ -23,7 +23,7 @@ const content = [
     title: "API Data",
     href: "https://github.com/malarm/React-With-API",
     description:
-      "This website is made for a company which rents out summer houses to its clients. There are two main features in this website. ",
+      "A website to a company that rents out summer houses Two main features : The first feature is to demonstrate property details. The second feature is to show the trarrif range in the filter. ",
     project: "Personal",
     usedTechnologies: (
       <>
@@ -36,7 +36,7 @@ const content = [
     title: "Malar Resort",
     href: "https://github.com/malarm/malarresort",
     description:
-      " I developed a website for a resort. The search function is the main function of the website. Hotel rooms can be selected based on room type ,number of guests and the price of the rooms. Web API is an important technical feature.",
+      " Developed this website for a resort having the search function as the vital function. Web API is an important feature.",
 
     project: "Personal",
     usedTechnologies: (
@@ -52,7 +52,7 @@ const content = [
     title: "IMDB Movies",
     href: "https://github.com/malarm/IMDB",
     description:
-      "This website is made for a company which rents out summer houses to its clients. There are two main features in this website. ",
+      "Developed this website to  learn to use web API. The website has a search function where we can insert one or many IMDB IDs.  ",
     project: "Personal",
     usedTechnologies:
       "C#, ASP.NET.Core, API, Entity framework, MS SQL, Bootstrap",
@@ -62,7 +62,7 @@ const content = [
     title: "Deo",
     href: "https://www.deo.dk/",
     description:
-      "This website is made for a company which rents out summer houses to its clients. There are two main features in this website. ",
+      "Built this website and maintained on a daily basis and making the registration page for participants was an integral part of work. ",
     project: "Work",
     usedTechnologies:
       "C#, ASP.NET MVC, Umbraco, Visual studio, Entity framework, SQL, Html5, CSS3, Javascript",
@@ -72,7 +72,7 @@ const content = [
     title: "Undervisning",
     href: "http://www.undervisning.deo.dk/",
     description:
-      "This website is made for a company which rents out summer houses to its clients. There are two main features in this website. ",
+      "I worked on this project from the beginning to end singlehandedly.  ",
     project: "Work",
     usedTechnologies:
       "C#, ASP.NET MVC, Umbraco, Visual studio, SQL, Html5, CSS3, Javascript",
@@ -81,8 +81,7 @@ const content = [
     imgSrc: Euforedrag,
     title: "Euforedrag",
     href: "http://www.euforedrag.dk/",
-    description:
-      "This website is made for a company which rents out summer houses to its clients. There are two main features in this website. ",
+    description: "I took care of the maintenance work. ",
     project: "Work",
     usedTechnologies:
       "C#, ASP.NET MVC, Umbraco, Visual studio, SQL, Html5, CSS3",
@@ -91,8 +90,7 @@ const content = [
     imgSrc: Notat,
     title: "Notat",
     href: "http://notat.dk/",
-    description:
-      "This website is made for a company which rents out summer houses to its clients. There are two main features in this website. ",
+    description: "I developed  and maintained  this project . ",
     project: "Work",
     usedTechnologies:
       "C#, ASP.NET MVC, Umbraco, Visual studio, SQL, Html5, CSS3",
@@ -102,7 +100,7 @@ const content = [
     title: "Cphcasting",
     href: "https://www.cphcasting.dk/",
     description:
-      "This website is made for a company which rents out summer houses to its clients. There are two main features in this website. ",
+      "I was part of a team of two people who developed and maintained this big project. I developed several modules and contributed significantly to build search function which is an important feature.  ",
     project: "Work",
     usedTechnologies: "PHP, Drupal, Wordpress , HTML, CSS, Javascript",
   },
