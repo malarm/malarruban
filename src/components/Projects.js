0865c649863c8a64b0a2b98cfc11ebd9aaaad911
malarm <malarm09@gmail.com>
2020-04-21 10:43:25 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,19 +15,17 @@ function Projects() {
               <label>{project}</label>
               <div className="text-block">
                 <h4>{title}</h4>
-                <Button>
-                   <a href={href} target="_blank" className="viewproject-button">
-                    View project
-                    <span>
-                      <FaArrowRight />
-                    </span>
-                  </a>
-                </Button>
               </div>
-              <div className="middle">
+              <div className="overlay">
                 <div className="project-desc">{description}</div>
                 <div className="used-tech">
                   Used Technologies : {usedTechnologies}
+                </div>
+                <div className="viewproject-button">
+                  View project
+                  <span>
+                    <FaArrowRight />
+                  </span>
                 </div>
               </div>
             </Card>
