9ec44f7fe6a72286ddd11162da7e6f5f56402cc1
malarm <malarm09@gmail.com>
2020-04-26 10:38:02 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,13 +13,12 @@ function Projects() {
             <Card className="container">
               <a href={href} target="_blank">
                 <img src={imgSrc} />
-
                 <label>{project}</label>
                 <div className="text-block">
                   <h4>{title}</h4>
                 </div>
 
-                <div className="overlay">
+                <div className="middle">
                   <div className="project-desc">{description}</div>
                   <div className="used-tech">
                     <ul>
