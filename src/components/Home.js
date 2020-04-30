af06d72ac00538195ce2a8d4bc0cad04199dc2db
malarm <malarm09@gmail.com>
2020-04-30 21:25:23 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Banner from './Banner';
-import Hero from './Hero';
-import About from './About';
-import Projects from './Projects';
+import React from "react";
+import Banner from "./Banner";
+import Hero from "./Hero";
+import About from "./About";
+import Projects from "./Projects";
 
 const Home = () => {
-    return (
-        <>
-
-            <Hero hero="bannerhero" >
-                <Banner>
-                    <h1>Hello , I am <span style={{ color: '#ff2058' }}>Malar</span> welcome to my world</h1>
-                    <h4 style={{ color: '#343a40' }}>Front-end developer</h4>
-                </Banner>
-            </Hero>
-            <About />
-            <Projects />
-
-        </>
-    )
-
-}
-export default Home
+  return (
+    <>
+      <Hero hero="bannerhero">
+        <Banner>
+          <h1 style={{ color: "#ffffff", marginTop: "60px" }}>
+            Hello, I am <span style={{ color: "#ff2058" }}>Malar</span> welcome
+            to my world
+          </h1>
+          <h4 style={{ color: "#ffffff" }}>Front-end developer</h4>
+        </Banner>
+      </Hero>
+      <About />
+      <Projects />
+    </>
+  );
+};
+export default Home;
