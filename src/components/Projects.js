a6fdababd474eeff417d7be94519b3e9e2224a95
malarm <malarm09@gmail.com>
2020-05-09 12:29:22 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,15 +5,20 @@ import { FaArrowRight } from "react-icons/fa";
 function Projects() {
   return (
     <div className="project-container">
-      <h1 className="project-title">Projects</h1>
+      <h1>Projects </h1>
+      <h3>
+        Professionally and personally I push myself to learn,
+        <br /> and continously get better at what i love – building things.
+      </h3>
+
       <div className="projects">
         {Content.map(
           ({ imgSrc, title, href, description, project, usedTechnologies }) => (
             <div className="container">
               <a href={href} target="_blank">
                 <img src={imgSrc} />
-                <label>{project}</label>
                 <div className="text-block">
+                  <label>{project}</label>
                   <h4>{title}</h4>
                 </div>
                 <div className="overlay">
@@ -25,12 +30,12 @@ function Projects() {
                       ))}
                     </ul>
                   </div>
-                  <div className="viewproject-button">
+                  {/* <div className="viewproject-button">
                     <div>View Project</div>
                     <div style={{ padding: "3px" }}>
                       <FaArrowRight />
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </a>
             </div>
