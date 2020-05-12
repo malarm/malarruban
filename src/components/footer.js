b41db948af365ba72a6099ac7189f417769f6d8f
malarm <malarm09@gmail.com>
2020-05-12 08:36:42 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,17 +9,12 @@ function Footer() {
       <h3>malam09@gmail.com</h3>
       <h3>+45 91 73 20 19</h3>
       <h3>
-        <a
-          href="https://github.com/malarm?tab=repositories"
-          target="_blank"
-          style={{ color: "black" }}
-        >
+        <a href="https://github.com/malarm?tab=repositories" target="_blank">
           <IoLogoGithub />
         </a>
         <a
           href="https://www.linkedin.com/in/malar-ruban-a02669b5/"
           target="_blank"
-          style={{ color: "black" }}
         >
           <IoLogoLinkedin />
         </a>
