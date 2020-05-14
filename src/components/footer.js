a458e559e34d80b5e02f168ee1ae8241ea343fa1
malarm <malarm09@gmail.com>
2020-05-14 09:47:18 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
 
-function Footer() {
+function Projects() {
   return (
     <div className="footer">
-      <h1>I dont'n drink coffee, but I love tea. let's talk.</h1>
+      <h1>I don't drink coffee, but I love tea. let's talk.</h1>
       <h3>Malar Ruban</h3>
       <h3>malam09@gmail.com</h3>
       <h3>+45 91 73 20 19</h3>
@@ -23,4 +23,4 @@ function Footer() {
   );
 }
 
-export default Footer;
+export default Projects;
