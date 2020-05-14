450b41fc94674a6a51ef3eaf53340e923a2760b3
malarm <malarm09@gmail.com>
2020-05-14 09:50:53 +0200

    Update TabList.js

--- src/components/TabList.js
+++ src/components/TabList.js
@@ -17,12 +17,10 @@ const Skills = () => {
             where I’m experienced in HTML5, CSS3 and Javascript, incl.frameworks
             like React.js and Next.js.
           </p>
-          <br />
           <p>
             <b>Full-stack perspective.</b> I also have experience working with
             backend, databases, 3rd party integrations and infrastructure.
           </p>
-          <br />
           <p>
             <b> CMS.</b> I spent years working in Umbraco , Drupal and Wordpress
             setups and know my way around.
@@ -30,41 +28,27 @@ const Skills = () => {
         </TabPanel>
         <TabPanel>
           <p>
-            <b>Front-end developer - Intern</b> - Appliedtechnologies
-            <br />
-            2019 - 2020
+            <b>Front-end developer - Intern</b> - Appliedtechnologies 2019 -
+            2020
           </p>
-          <br />
           <p>
-            <b>Web developer</b> - DEO
-            <br />
-            2017 - 2018
+            <b>Web developer</b> - DEO 2017 - 2018
           </p>
-          <br />
           <p>
-            <b>Web developer</b> - Xenos IT
-            <br />
-            2014
+            <b>Web developer</b> - Xenos IT 2014
           </p>
-          <br />
           <p>
-            <b>Developer</b> - Usoftware Systems
-            <br />
-            2012 - 2014
+            <b>Developer</b> - Usoftware Systems 2012 - 2014
           </p>
         </TabPanel>
         <TabPanel>
           <p>
             <b>Master in computer application, MSc</b> - Bharathidasan
-            University
-            <br />
-            2007 - 2010
+            University 2007 - 2010
           </p>
-          <br />
           <p>
-            <b>Bachelor in Chemistry, BSc</b> - Bharathidasan University
-            <br />
-            2004 - 2007
+            <b>Bachelor in Chemistry, BSc</b> - Bharathidasan University 2004 -
+            2007
           </p>
         </TabPanel>
       </TabPanels>
