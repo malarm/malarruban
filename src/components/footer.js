574c7ddb94de22528201eee0a8789b325ef14c51
malarm <malarm09@gmail.com>
2020-10-07 13:34:47 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className="footer">
       <h1>I don't drink coffee, but I love tea. Let's talk.</h1>
       <p>Malar Ruban</p>
-      <p>malam09@gmail.com</p>
+      <p>malarm09@gmail.com</p>
       <p>+45 91 73 20 19</p>
       <p style={{ padding: "5px" }}>
         <a href="https://github.com/malarm?tab=repositories" target="_blank">
